fa4be47773b17cb70154b41396c771b2693ed70a
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2021-12-01 17:39:12 +0100

    Create functions0.py

---
+++ functions0.py
@@ -0,0 +1,88 @@
+from _up import *
+
+
+running = False
+stop()
+save()
+start()
+running = True
+print("Reset complete!")
+
+# do sth
+
+running = False
+stop()
+save()
+start()
+running = True
+print("Reset complete!")
+
+# do sth
+
+running = False
+stop()
+save()
+start()
+running = True
+print("Reset complete!")
+
+# do sth
+
+running = False
+stop()
+save()
+start()
+running = True
+print("Reset complete!")
+
+print(running)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def reset():
+    running = False
+    stop()
+    save()
+    start()
+    running = True
+    print("Reset complete!")
+
+
+reset()
+
+reset()
+
+reset()
+
+
+
+
+
+
+
+
+
+
+
