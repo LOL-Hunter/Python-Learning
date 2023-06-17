25aa0e7fa1da1648e28025fd879ce32ae209f5c3
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2023-06-17 15:00:51 +0200

    Update Float.py

--- Float.py
+++ Float.py
@@ -1,11 +1,44 @@
-f = 1.554
+package de.hunter.Listeners;
 
-a = round(f, 0) # 2.0
+import de.hunter.Managers.AllItemsManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-b = round(f, 1) # 1.6
+public class on_playerInventoryClickEvent implements Listener {
 
-c = round(f, 2) # 1.55
+    @EventHandler
+    public static void onEvent(InventoryClickEvent e){
+        Player p = (Player) e.getWhoClicked();
 
-d = int(f) # 1
+        if (p.getOpenInventory().getTitle().startsWith("View")){
+            e.setCancelled(true);
+            int page = Integer.parseInt(p.getOpenInventory().getTitle().split("_")[1]);
+            if (e.getCurrentItem() == null) return;
+            if (!e.getCurrentItem().hasItemMeta()) return;
+            if (!e.getCurrentItem().getItemMeta().hasDisplayName()) return;
 
-e = 4 / 2 # 2.0    (immer Float Achtung -> ggf round bzw int)
+            if (e.getCurrentItem().getItemMeta().getDisplayName().equals("§cBack")){
+                if (page == 0) return;
+                changeInventory(e.getInventory(), page-1);
+            } else if (e.getCurrentItem().getItemMeta().getDisplayName().equals("§aForward")){
+                changeInventory(e.getInventory(), page+1);
+            }
+        }
+    }
+    public static void changeInventory(Inventory inv, int page){
+        int slots = 45;
+        if (AllItemsManager.submitted.get(slots*page) == null) return;
+        //set title
+        for (int i=0; i<=slots-1; i++){
+            Material m = AllItemsManager.submitted.get(i+(slots*page));
+            if (m == null) continue;
+            inv.setItem(i, new ItemStack(m));
+        }
+
+    }
+}
